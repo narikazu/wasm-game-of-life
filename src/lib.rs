72b50239aa3ace1ed117710c426413b67eac19b9
Kazunari Okuda <saori.cube@gmail.com>
2019-09-07 23:32:58 +0200

    Create Cell enum

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,11 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
 }
 
 #[wasm_bindgen]
